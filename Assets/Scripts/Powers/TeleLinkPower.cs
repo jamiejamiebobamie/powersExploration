b36b1ddc3ca80cde5ee0d9eecc73dccfc76c1d6d
jamiejamiebobamie <jmccrory@vt.edu>
2019-11-21 23:00:09 -0800

    attempting to implement a lerp-to-closest-teleport-waypoint-in-line-of-sight power

---
+++ Assets/Scripts/Powers/TeleLinkPower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleLinkPower : MonoBehaviour, IPowerable
+{
+    private void Start()
+    {
+        TeleLinkObject[] teleportLocations = FindObjectsOfType<TeleLinkObject>();
+    }
+
+    public void ActivatePower()
+    {
+        return;
+    }
+}
---
+++ Assets/Scripts/World/TeleLinkObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleLinkObject : MonoBehaviour, ITeleportable
+{
+    private void Start()
+    {
+       GameObject[] allObjectsInLevel = FindObjectsOfType<GameObject>();
+
+        foreach (GameObject o in allObjectsInLevel)
+        {
+            if (o.GetComponent<TeleLinkPower>() != null)
+            {
+                return;//idk too tired.
+            }
+        }
+        
+    }
+    public float ReturnDistance()
+    {
+        return transform.position;
+    }
+}
